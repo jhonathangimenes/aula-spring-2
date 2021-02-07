d0e0de5b97e77b22235fe06f2a3b6d48a6a21769
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-07 19:39:59 -0300

    inserir usuario

--- src/main/java/com/example/aulaspring2/services/UserService.java
+++ src/main/java/com/example/aulaspring2/services/UserService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.aulaspring2.dto.UserDTO;
 import com.example.aulaspring2.entities.User;
 import com.example.aulaspring2.repositories.UserRepository;
 import com.example.aulaspring2.services.exceptions.ObjectNotFoundException;
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
