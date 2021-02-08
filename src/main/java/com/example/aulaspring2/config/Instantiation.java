a0f11a6c15744c07e7584b7e56f0821396dfdc65
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-07 21:24:00 -0300

    Inclusão authorDTO

--- src/main/java/com/example/aulaspring2/config/Instantiation.java
+++ src/main/java/com/example/aulaspring2/config/Instantiation.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.aulaspring2.dto.AuthorDTO;
 import com.example.aulaspring2.entities.Post;
 import com.example.aulaspring2.entities.User;
 import com.example.aulaspring2.repositories.PostRepository;
@@ -35,11 +36,13 @@ public class Instantiation implements CommandLineRunner {
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2021"), "Partiu viagem", "Muito bom viagem", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2021"), "Foto Estrelada", "Melhores fotos", maria);
-		Post post3 = new Post(null, sdf.parse("21/03/2021"), "Carro do ano", "Rapido com nunca", bob);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/01/2021"), "Partiu viagem", "Muito bom viagem", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("25/02/2021"), "Foto Estrelada", "Melhores fotos", new AuthorDTO(maria));
+		Post post3 = new Post(null, sdf.parse("30/03/2021"), "Carro do ano", "Rapido com nunca", new AuthorDTO(bob));
+		
+
 		postRepository.saveAll(Arrays.asList(post1, post2, post3));
 	}
 
