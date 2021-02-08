27fe15ab9d1eb4b69bcf9d6c6dfb2b83dd39116d
Jhonathan Gimenes <gimenesjhonathan@gmail.com>
2021-02-07 21:43:47 -0300

    buscar post por id

---
+++ src/main/java/com/example/aulaspring2/resources/PostResource.java
@@ -0,0 +1,25 @@
+package com.example.aulaspring2.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.aulaspring2.entities.Post;
+import com.example.aulaspring2.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+	
+	@Autowired
+	private PostService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+}
---
+++ src/main/java/com/example/aulaspring2/services/PostService.java
@@ -0,0 +1,22 @@
+package com.example.aulaspring2.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aulaspring2.entities.Post;
+import com.example.aulaspring2.repositories.PostRepository;
+import com.example.aulaspring2.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
